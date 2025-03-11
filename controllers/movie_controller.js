efe572c920c2bce0d7d5784e351393e2e9aa8f2c
Drak03 <drak.rocz@gmail.com>
2025-03-11 19:03:26 +0530

    add controller files

---
+++ controllers/movie_controller.js
@@ -0,0 +1,20 @@
+  const movieIndex =(req, res) => {
+    res.send('hello')
+}  
+
+const movieCreate = (req,res)=>{
+    res.send('hello')
+}
+
+const movieUpdate = (req,res)=>{
+    res.send('hello')
+}
+
+const movieDelete = (req,res)=>{
+    res.send('hello')
+}
+
+
+  module.exports = {
+    movieIndex,movieCreate,movieUpdate,movieDelete,
+  }
--- routes/movies_routes.js
+++ routes/movies_routes.js
@@ -1,26 +1,20 @@
 const express = require('express'); 
+const { movieIndex,movieCreate,movieUpdate,movieDelete } = require('../controllers/movie_controller');
 
 const router = express.Router()
 
 //CRUD functions for movies 
 
 // for Reading the movies 
-router.get('/movies', (req, res) => {
-    res.send('hello')
-})
+router.get('/movies', movieIndex)
 
 // for Creating movies
-router.post('/movies',(req,res)=>{
-    res.send('hello')
-})
+router.post('/movies',movieCreate)
 
 // for updating movies 
-router.put('/movies/:id',(req,res)=>{
-    res.send('hello')
-})
+router.put('/movies/:id',movieUpdate)
 
 // for deleting movies 
-router.delete('/movies/:id',(req,res)=>{
-    res.send('hello')
-})
+router.delete('/movies/:id',movieDelete)
+
 module.exports = router;
