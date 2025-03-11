1557d3e6d8267163239d57876ce8b88ced97b6b9
Drak03 <drak.rocz@gmail.com>
2025-03-11 16:47:48 +0530

    crud routes

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ app.get('/', (req, res) => {
   res.json({msg: 'Hello World!'})
 })
 
+
+//CRUD functions for movies 
+
+// for Reading the movies 
+app.get('/movies', (req, res) => {
+  })
+
+// for Creating movies
+app.post('/movies',(req,res)=>{
+
+})
+
+// for updating movies 
+app.put('/movies/:id',(req,res)=>{
+
+})
+
+// for deleting movies 
+app.delete('/movies/:id',(req,res)=>{
+
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
